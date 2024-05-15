4304ca0f1a2539d98b4b1d9b80b683553059ea84
LuaNery <luananery1@hotmail.com>
2024-05-15 11:10:20 -0300

    primeiro commit

---
+++ Database/config.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class config {
+    public static final String HOST = "localhost";
+    public static final String USER = "root";  
+    public static final String PASSWORD = "";
+    public static final String DATABASE = "usuarios";
+}
---
+++ Database/database.java
@@ -0,0 +1,12 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class database {
+    private static final String HOST = "localhost";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    public static final String DATABASE = "usuarios";
+}
